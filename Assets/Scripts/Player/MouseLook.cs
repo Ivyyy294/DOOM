55176bebf00d2b82f05e539f26947a45ec265ef5
Ivyyy <48586445+Ivyyy294@users.noreply.github.com>
2023-04-06 22:26:10 +0200

    Door prefabs, CrossHair

--- Assets/Scripts/Player/MouseLook.cs
+++ Assets/Scripts/Player/MouseLook.cs
@@ -14,7 +14,7 @@ public class MouseLook : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-		//Cursor.lockState = CursorLockMode.Locked;
+		Cursor.lockState = CursorLockMode.Locked;
 		cameraTrans = Camera.main.transform;
     }
 
